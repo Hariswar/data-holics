d055d28a3a74aa5eb4d84510f3264135c5812043
Michael <mjhf89@umsystem.edu>
2025-04-30 02:10:20 -0500

    Made it so that when you go to the port it redirects you

--- dbmanage/urls.py
+++ dbmanage/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("leaguify/", include("leaguify.urls")),
+    path("", include("leaguify.urls")),
 ]
