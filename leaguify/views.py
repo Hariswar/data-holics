75ff8b60c44a33c74e276057468bb32c9e534f0d
ewolf <elijahwolf05@icloud.com>
2025-04-30 15:12:30 -0500

    uhh a lot of things

--- leaguify/views.py
+++ leaguify/views.py
@@ -6,11 +6,45 @@ from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import authenticate, login, logout, get_user
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-# from django.contrib.auth.models import User
+from django.views.generic.detail import DetailView
 from .models import *
 from .forms import *
 
 # Create your views here.
+
+# DETAIL VIEWS
+
+class LeagueDetailView(DetailView):
+    model = League
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        teams = Team.objects.filter(leagueID_id=context['object'].id)
+        context['teams'] = teams
+        return context
+class TeamDetailView(DetailView):
+    model = Team
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        players = Player.objects.filter(teamID_id=context['object'].id)
+        context['players'] = players
+        if players.filter(id=self.request.user.id).count() > 0:
+            context['team_joinable'] = False
+        else:
+            players2 = Player.objects.filter(userID_id=self.request.user.id, teamID_id=context['object'].id)
+            if players2.count() > 0:
+                context['team_joinable'] = False
+            else:
+                context['team_joinable'] = True
+        return context
+
+class PlayerDetailView(DetailView):
+    model = Player
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 # --- HTML PAGE VIEWS ---
 
@@ -38,9 +72,11 @@ def loginPage(request):
         #     user = None
         # context = {'page':page}
     return render(request, 'login.html')
+
 def logoutUser(request):
     logout(request)
     return redirect('login')
+
 def registerPage(request):
     if request.method == 'POST':
         try:
@@ -89,26 +125,45 @@ def create_acct(request):
 # CREATE NEW LEAGUE PAGE
 @csrf_protect
 def create_league(request):
-    sports = Sport.objects.all()
-    template = loader.get_template('create_league.html')
-    context = {
-        'sports': sports
-    }
-    return HttpResponse(template.render(context, request))
+    if request.method == 'GET':
+        sports = Sport.objects.all()
+        template = loader.get_template('create_league.html')
+        context = {
+            'sports': sports
+        }
+        return HttpResponse(template.render(context, request))
+    elif request.method == 'POST':
+        leagueName = request.POST.get('leagueName')
+        sportID = request.POST.get('sport')
+        teamName = request.POST.get('yourTeamName')
+        try:
+            league = League.objects.create(leagueName=leagueName, sportID_id=sportID)
+            team = Team.objects.create(teamName=teamName, leagueID_id=league.id)
+            player = Player.objects.create(teamID_id=team.id, userID_id=request.user.id)
+            return redirect('user_home')
+        except Exception as e:
+            return redirect('create_league')
 
 # CREATE NEW TEAM PAGE
 @csrf_protect
-def create_team(request):
-    template = loader.get_template('create_team.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+def create_team(request, pk):
+    if request.method == 'GET':
+        template = loader.get_template('create_team.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+    elif request.method == 'POST':
+        teamName = request.POST.get('teamName')
+        team = Team.objects.create(teamName=teamName, leagueID_id=pk)
+        player = Player.objects.create(teamID_id=team.id, userID_id=request.user.id)
+        return redirect('.')
+    return redirect('.')
 
 # USER HOME PAGE
 @login_required
 def user_home(request):
     user = Custom_User.objects.get(emailAddress=request.user)
     template = loader.get_template('user_home.html')
-    players = Player.objects.filter(id=request.user.id)
+    players = Player.objects.filter(userID_id=request.user.id)
     teamIDs = players.values('teamID_id')
     teams = []
     for item in teamIDs: 
@@ -121,7 +176,6 @@ def user_home(request):
         "teams": teams
     }
     return HttpResponse(template.render(context, request))
-
 
 # --- DATABASE FUNCTIONS + REDIRECTS ---
 
@@ -169,6 +223,14 @@ def create_new_league(request):
         except Exception as e:
             return redirect('create_league')
     return render(request, 'blank.html')
+
+@csrf_protect 
+def join_team(request, pk):
+    team = Team.objects.get(pk=pk)
+    user = Custom_User.objects.get(id=request.user.id)
+    print(user, team)
+    player = Player.objects.create(userID=user, teamID=team)
+    return redirect('.')
 
 @login_required
 def create_new_sport(request):
